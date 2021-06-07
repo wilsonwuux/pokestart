f0317e84bdbf4ddee4bb40a8844c4700739c0882
wilsonwuux <71638140+wilsonwuux@users.noreply.github.com>
2021-06-06 23:34:18 -0600

    c Control
    Changed Control

--- src/Components/ControlPokes.js
+++ src/Components/ControlPokes.js
@@ -4,11 +4,11 @@ import {Button,ButtonGroup} from 'react-bootstrap'
 const ControlPokes=({setSlice,Slice,Newitems})=>{
     return <ButtonGroup className=' col-auto ' aria-label="Basic example">
                 <Button onClick={()=>{(Newitems.length&&(Newitems.length>=5))?setSlice(Slice-10):alert('No hay mas elementos' )}} variant="danger"> 
-                    <i className='fa fa-plus'></i>
+                    <i className='fa fa-minus'></i>
                 </Button>
                     <Button variant="dark"> POKEMONS </Button>
                 <Button onClick={()=>{(Slice===942)?alert('Todos los datos fueron cargados'):setSlice(Slice+10)}} variant="success">
-                    <i className='fa fa-minus'></i>
+                    <i className='fa fa-plus'></i>
                 </Button>
         </ButtonGroup>
 }
